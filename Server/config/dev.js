cf4223b1c229d8d68194fca093daea40358d6ad9
divyanshu <b17084@students.iitmandi.ac.in>
2020-08-27 23:03:13 +0530

    final3

---
+++ Server/config/dev.js
@@ -0,0 +1,6 @@
+module.exports={
+    MONGOURI:"mongodb+srv://divyanshu:Etn2LM0EuS7WN9CV@cluster0-iqpwu.mongodb.net/instagram?retryWrites=true&w=majority",
+    JWT_SECRET:"edfgdyfgdfhdshvjer",
+    SENDGRID_API:"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",
+    EMAIL:"http:localhost:3000"
+}
